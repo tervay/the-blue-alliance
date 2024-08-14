6e79562c7d27441aec9a1e16f83c4a3c3cd3dbfe
Justin Tervay <justin@tervay.com>
2024-08-14 16:55:34 -0400

    [PWA] Refactor match tables. Add playofftype. Minor styling adjustments. Add scrollarea component.

--- pwa/.eslintrc.cjs
+++ pwa/.eslintrc.cjs
@@ -122,6 +122,7 @@ module.exports = {
               'size',
               'sideOffset',
               'shouldScaleBackground',
+              'orientation',
             ],
           },
         ],

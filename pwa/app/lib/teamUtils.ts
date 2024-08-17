ad30dad69e6622f526c74147baddcc1c1775a3f9
Justin Tervay <justin@tervay.com>
2024-08-17 13:33:28 -0400

    [PWA] Add vitest; add team sponsor parsing & accordion

--- pwa/app/lib/teamUtils.ts
+++ pwa/app/lib/teamUtils.ts
@@ -9,3 +9,26 @@ export function sortTeamsComparator(a: Team, b: Team) {
 export function sortTeamKeysComparator(a: string, b: string) {
   return Number(removeNonNumeric(a)) - Number(removeNonNumeric(b));
 }
+
+// Known problem: Does not work when the final company listed has an ampersand in its name
+// For example: Company 1/Company 2 & 3&Public School
+// becomes ["Company 1", "Company 2"]
+export function attemptToParseSponsors(teamName: string): string[] {
+  const familyCommunity = 'Family/Community';
+  if (teamName.endsWith(familyCommunity)) {
+    teamName = teamName.substring(0, teamName.length - familyCommunity.length);
+  }
+
+  const lastSlash = Math.max(teamName.lastIndexOf('/'), 0);
+  const firstAmpersand = teamName.substring(lastSlash).indexOf('&');
+  const sponsorCompanySection = teamName.substring(
+    0,
+    lastSlash + firstAmpersand,
+  );
+
+  const sponsors = sponsorCompanySection
+    .split('/')
+    .map((s) => s.trim())
+    .filter((s) => s.length > 0);
+  return sponsors;
+}
--- pwa/app/routes/team.$teamNumber.tsx
+++ pwa/app/routes/team.$teamNumber.tsx
@@ -19,6 +19,14 @@ import { getTeam, getTeamMediaByYear } from '~/api/v3';
 import InlineIcon from '~/components/tba/inlineIcon';
 import TeamAvatar from '~/components/tba/teamAvatar';
 import TeamRobotPicsCarousel from '~/components/tba/teamRobotPicsCarousel';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '~/components/ui/accordion';
+import { Badge } from '~/components/ui/badge';
+import { attemptToParseSponsors } from '~/lib/teamUtils';
 
 async function loadData(params: Params) {
   if (params.teamNumber === undefined) {
@@ -92,6 +100,8 @@ export default function TeamPage(): JSX.Element {
     [media],
   );
 
+  const sponsors = attemptToParseSponsors(team.name);
+
   return (
     <div className="flex flex-wrap justify-center">
       <div className="basis-1/2">
@@ -105,10 +115,29 @@ export default function TeamPage(): JSX.Element {
           {team.city}, {team.state_prov}, {team.country}
         </InlineIcon>
 
-        <InlineIcon displayStyle={'flexless'}>
-          <BiInfoCircleFill />
-          {team.name}
-        </InlineIcon>
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1" className="border-0">
+            <AccordionTrigger className="p-0 text-left font-normal">
+              <InlineIcon displayStyle={'flexless'}>
+                <BiInfoCircleFill />
+                {team.school_name ?? 'Family/Community'}
+                {sponsors.length > 0 && ` with ${sponsors.length} sponsor`}
+                {sponsors.length > 1 && 's'}
+              </InlineIcon>
+            </AccordionTrigger>
+            <AccordionContent className="pb-0">
+              {sponsors.map((sponsor, i) => (
+                <Badge
+                  className="m-px font-normal"
+                  key={i}
+                  variant={'secondary'}
+                >
+                  {sponsor}
+                </Badge>
+              ))}
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
 
         <InlineIcon>
           <BiCalendar />

269e496d2475c8e5da063f1c2a7c0bab3f3e0ad7
Justin Tervay <justin@tervay.com>
2024-08-17 17:08:32 -0400

    [PWA] Add team socials to page, minor styling tweaks

--- pwa/app/components/tba/teamRobotPicsCarousel.tsx
+++ pwa/app/components/tba/teamRobotPicsCarousel.tsx
@@ -1,7 +1,6 @@
 import Autoplay from 'embla-carousel-autoplay';
 
 import { Media } from '~/api/v3';
-import { Card, CardContent } from '~/components/ui/card';
 import {
   Carousel,
   CarouselContent,
@@ -16,29 +15,24 @@ export default function TeamRobotPicsCarousel({
   media: Media[];
 }): JSX.Element {
   return (
-    <Carousel
-      className="mx-12 w-full max-w-xs"
-      plugins={[Autoplay({ delay: 5000 })]}
-    >
-      <CarouselContent>
+    <Carousel className="w-full max-w-xs" plugins={[Autoplay({ delay: 5000 })]}>
+      <CarouselContent className="items-center">
         {media.map((m, index) => (
           <CarouselItem key={index}>
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <img
-                    className="size-full object-cover"
-                    src={m.direct_url}
-                    alt={''}
-                  />
-                </CardContent>
-              </Card>
+            <div className="rounded-lg border-4 border-neutral-600">
+              <img
+                className="rounded object-cover"
+                src={m.direct_url}
+                alt={''}
+              />
             </div>
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
+      <div className="mt-1 flex justify-center gap-8">
+        <CarouselPrevious className="relative left-0 transform-none" />
+        <CarouselNext className="relative right-0 transform-none" />
+      </div>
     </Carousel>
   );
 }
